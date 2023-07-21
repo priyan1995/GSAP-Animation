cce27e2b805cd9d10c6feb3e6e868dfcd560e02b
priyan <priyan.abewickrama@ideahub.lk>
2023-07-21 16:07:06 +0530

    scroll trigger toggle class

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,16 @@ anim1.fromTo( bannerImage, 1, { height:"0" }, {height:"80vh", ease: Power2.easeI
 
 .fromTo( gradiant, 1.2, { x: "-100%" }, { x:"0%", ease: Power2.easeInOut } )
 .fromTo ( bannerTitle, 1.5 , { x:"50%", opacity: 0 }, { x:"0%", opacity: 1, ease: Power2.easeInOut } )
+
+
+// toggle class
+gsap.registerPlugin(ScrollTrigger);
+
+ScrollTrigger.create({
+    markers:true,
+    start: "top 70%",
+    end: "top 0%",
+    trigger: '.st-toggle-sec-2',
+    toggleClass: { targets: ".st-toggle-sec-2", className: "pd-sec-light"},
+    ease: Power2.easeInOut
+})
