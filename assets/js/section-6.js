78deacd6ac0b8b6779ecb111d2c858b87b5cfc10
Priyan Darshana <45042030+priyan1995@users.noreply.github.com>
2023-09-11 12:50:21 +0530

    text highlight issues fixed

--- assets/js/section-6.js
+++ assets/js/section-6.js
@@ -18,19 +18,19 @@
 
 
     
-gsap.set('.pd-text-w', {width:0, transformOrigin:'left center'})
+gsap.set('.pd-text-w', {scaleX:'0%', transformOrigin:'left center'})
 
 gsap.timeline({
     scrollTrigger:{
       trigger: '.pd-text-w',
       scrub:0.3,
-      start:"top 40%",
+      start:"top 70%",
       end:"top 20%",
       markers:true
     }
  })
   .to('.pd-text-w', {
-  width:'100%', 
+    scaleX:'100%', 
   duration:1, ease:'none',
   stagger:1
 })
