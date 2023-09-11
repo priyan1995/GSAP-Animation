4694496b793b80cb4454b351b9f64c543119e843
Priyan Darshana <45042030+priyan1995@users.noreply.github.com>
2023-09-11 12:50:39 +0530

    code clear

--- assets/js/section-6.js
+++ assets/js/section-6.js
@@ -1,22 +1,5 @@
 
 
-// ============== ipod large
-// const tltxtwhite = gsap.timeline({
-//     scrollTrigger: {
-//         trigger: '.pd-txt-white',
-//         markers: true,
-//         start: "top 70%",
-//         end: "bottom -240%",
-//         scrub: true,
-//         toggleActions: "restart resume resume reverse",
-//     }
-// })
-
-//     .to('.pd-txt-white', { duration: 1, color: 'white', opacity: 1 })
-
- 
-
-
     
 gsap.set('.pd-text-w', {scaleX:'0%', transformOrigin:'left center'})
 
@@ -26,7 +9,7 @@ gsap.timeline({
       scrub:0.3,
       start:"top 70%",
       end:"top 20%",
-      markers:true
+     // markers:true
     }
  })
   .to('.pd-text-w', {
